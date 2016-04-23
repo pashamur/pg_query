9ea5d7ad3ac82e8247e57b206bcf125c126bb9cf
Pasha Muravyev <pasha@panjiva.com>
2016-04-22 23:52:41 -0400

    Add method simple_where_conditions to fetch conditions from a query

--- lib/pg_query/filter_columns.rb
+++ lib/pg_query/filter_columns.rb
@@ -1,4 +1,59 @@
 class PgQuery
+  class AExprParser
+    attr_reader :val, :var
+
+    def initialize(aexpr)
+      @aexpr = aexpr
+
+      if @aexpr["lexpr"].is_a?(Hash) && @aexpr["rexpr"].is_a?(Hash)
+        hsh = @aexpr["lexpr"].merge(@aexpr["rexpr"])
+
+        if hsh["A_Const"] && hsh["ColumnRef"]
+          @val = fetch_val(hsh["A_Const"]["val"])
+          @var = hsh["ColumnRef"]["fields"].map{|f| fetch_val(f)}.join(".")
+        else
+          @val = @var = nil
+        end
+      elsif get_name.downcase == "between"
+        @var = @aexpr["lexpr"]["ColumnRef"]["fields"].map{|f| fetch_val(f)}.join(".")
+        @val = @aexpr["rexpr"].map{|hsh| fetch_val(hsh["A_Const"]["val"]) }
+      else
+        @var = @val = nil
+      end
+    end
+
+    def fetch_val(segment)
+      if segment.keys[0].downcase == "string"
+        segment.values[0]["str"]
+      elsif segment.keys[0].downcase == "integer"
+        segment.values[0]["ival"]
+      elsif segment.keys[0].downcase == "float"
+        segment.values[0]["str"].to_f
+      end
+    end
+
+    def get_conditions
+      return nil if @var.nil? || @val.nil?
+      if get_name.downcase == "between"
+        [[get_var, ">=", get_val[0]], [get_var, "<=", get_val[1]]]
+      else
+        [get_var, get_name, get_val]
+      end
+    end
+
+    def get_name
+      fetch_val(@aexpr["name"].first)
+    end
+
+    def get_val
+      @val
+    end
+
+    def get_var
+      @var
+    end
+  end
+
   # Returns a list of columns that the query filters by - this excludes the
   # target list, but includes things like JOIN condition and WHERE clause.
   #
@@ -77,6 +132,91 @@ class PgQuery
     end
 
     filter_columns.uniq
+  end
+
+  def simple_where_conditions # rubocop:disable Metrics/CyclomaticComplexity
+    load_tables_and_aliases! if @aliases.nil?
+
+    # Get condition items from the parsetree
+    statements = @tree.dup
+    condition_items = []
+    filter_columns = []
+    conditions = []
+
+    loop do
+      statement = statements.shift
+      if statement
+        if statement[SELECT_STMT]
+          if statement[SELECT_STMT]['op'] == 0
+            if statement[SELECT_STMT][FROM_CLAUSE_FIELD]
+              # FROM subselects
+              statement[SELECT_STMT][FROM_CLAUSE_FIELD].each do |item|
+                next unless item['RangeSubselect']
+                statements << item['RangeSubselect']['subquery']
+              end
+
+              # JOIN ON conditions
+              condition_items += conditions_from_join_clauses(statement[SELECT_STMT][FROM_CLAUSE_FIELD])
+            end
+
+            # WHERE clause
+            condition_items << statement[SELECT_STMT]['whereClause'] if statement[SELECT_STMT]['whereClause']
+
+            # CTEs
+            if statement[SELECT_STMT]['withClause']
+              statement[SELECT_STMT]['withClause']['WithClause']['ctes'].each do |item|
+                statements << item['CommonTableExpr']['ctequery'] if item['CommonTableExpr']
+              end
+            end
+          elsif statement[SELECT_STMT]['op'] == 1
+            statements << statement[SELECT_STMT]['larg'] if statement[SELECT_STMT]['larg']
+            statements << statement[SELECT_STMT]['rarg'] if statement[SELECT_STMT]['rarg']
+          end
+        elsif statement['UpdateStmt']
+          condition_items << statement['UpdateStmt']['whereClause'] if statement['UpdateStmt']['whereClause']
+        elsif statement['DeleteStmt']
+          condition_items << statement['DeleteStmt']['whereClause'] if statement['DeleteStmt']['whereClause']
+        end
+      end
+
+      # Process both JOIN and WHERE conditions here
+      next_item = condition_items.shift
+      if next_item
+        if next_item[A_EXPR]
+          cond = AExprParser.new(next_item[A_EXPR]).get_conditions
+          if cond && cond[0].is_a?(Array)
+            conditions += cond
+          else
+            conditions << cond
+          end
+
+          %w(lexpr rexpr).each do |side|
+            expr = next_item.values[0][side]
+            next unless expr && expr.is_a?(Hash)
+            condition_items << expr
+          end
+        elsif next_item[BOOL_EXPR]
+          condition_items += next_item[BOOL_EXPR]['args']
+        elsif next_item[ROW_EXPR]
+          condition_items += next_item[ROW_EXPR]['args']
+        elsif next_item[COLUMN_REF]
+          column, table = next_item[COLUMN_REF]['fields'].map { |f| f['String']['str'] }.reverse
+          filter_columns << [@aliases[table] || table, column]
+        elsif next_item[NULL_TEST]
+          condition_items << next_item[NULL_TEST]['arg']
+        elsif next_item[FUNC_CALL]
+          # FIXME: This should actually be extracted as a funccall and be compared with those indices
+          condition_items += next_item[FUNC_CALL]['args'] if next_item[FUNC_CALL]['args']
+        elsif next_item[SUB_LINK]
+          condition_items << next_item[SUB_LINK]['testexpr']
+          statements << next_item[SUB_LINK]['subselect']
+        end
+      end
+
+      break if statements.empty? && condition_items.empty?
+    end
+
+    conditions.compact.uniq
   end
 
   protected
--- spec/lib/filter_columns_spec.rb
+++ spec/lib/filter_columns_spec.rb
@@ -5,6 +5,11 @@ def filter_columns(qstr)
   q.filter_columns
 end
 
+def simple_where_conditions(qstr)
+  q = PgQuery.parse(qstr)
+  q.simple_where_conditions
+end
+
 describe PgQuery, '#filter_columns' do
   it 'finds unqualified names' do
     expect(filter_columns('SELECT * FROM x WHERE y = ? AND z = 1')).to eq [[nil, 'y'], [nil, 'z']]
@@ -18,4 +23,36 @@ describe PgQuery, '#filter_columns' do
     query = 'WITH a AS (SELECT * FROM x WHERE x.y = ? AND x.z = 1) SELECT * FROM a WHERE b = 5'
     expect(filter_columns(query)).to match_array [['x', 'y'], ['x', 'z'], [nil, 'b']]
   end
+
+  it 'is able to extract simple integer where conditions' do
+    expect(simple_where_conditions('SELECT * FROM x WHERE y = 1 AND z = 2')).to eq [['y', '=', 1], ['z', '=', 2]]
+  end
+
+  it 'is able to extract simple string where conditions' do
+    expect(simple_where_conditions("SELECT * FROM x WHERE y > '1' AND z ~* 'small'")).to eq [['y', '>', '1'], ['z', '~*', 'small']]
+  end
+
+  it 'is able to extract simple where conditions with table' do
+    expect(simple_where_conditions("SELECT * FROM x WHERE x.y = 'test' AND z >= 53")).to eq [['x.y', '=', 'test'], ['z', '>=', 53]]
+  end
+
+  it 'is able to extract simple where conditions with table' do
+    expect(simple_where_conditions("SELECT * FROM x WHERE x.y = 'test' AND z >= 53")).to eq [['x.y', '=', 'test'], ['z', '>=', 53]]
+  end
+
+  it 'is able to parse contions out of CTEs' do
+    query = 'WITH a AS (SELECT * FROM x WHERE x.y = 10 AND x.z = 1) SELECT * FROM a WHERE b = 5'
+    expect(simple_where_conditions(query)).to eq [['b', '=', 5], ['x.y', '=', 10], ['x.z', '=', 1]]
+  end
+
+  it 'is able to parse floats and BETWEEN expressions' do
+    query = "SELECT * FROM x WHERE x.y = 5.3 AND z BETWEEN 5 AND 10"
+    expect(simple_where_conditions(query)).to eq [['x.y', '=', 5.3], ['z', '>=', 5], ['z', '<=', 10]]
+  end
+
+  it 'ignores typecasts' do
+    query = "SELECT * FROM x WHERE created_at > '2016-04-01'::date AND z = 10"
+    expect(simple_where_conditions(query)).to eq [['z', '=', 10]]
+  end
+
 end
